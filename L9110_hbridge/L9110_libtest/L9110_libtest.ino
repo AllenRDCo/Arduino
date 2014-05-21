21b733aa2bcb1365e21fb532522eebbf574d3e43
Paul Allen <jp.paul.allen@gmail.com>
2014-05-21 17:01:18 -0600

    Modified test sketch to test full range of options

--- L9110_hbridge/L9110_libtest/L9110_libtest.ino
+++ L9110_hbridge/L9110_libtest/L9110_libtest.ino
@@ -10,7 +10,20 @@ void setup()
 
 void loop()
 {
-  L9110.motorA(1,255);
+  L9110.motorA(1,255); //run both motors forward at full speed
   L9110.motorB(1,255);
+  delay(5000);
+  L9110.motorA(0,255); //run both motors backward at full speed
+  L9110.motorB(0,255);
+  delay(5000);
+  L9110.motorA(1,127); //run both motors forward at half speed
+  L9110.motorB(1,127);
+  delay(5000);
+  L9110.motorA(1,127); //run motors at half speed in opposite directions
+  L9110.motorB(0,127);
+  delay(5000);
+  L9110.motorA(0,0); //full stop on both motors
+  L9110.motorB(0,0);
+  delay(3000);
 }
 
