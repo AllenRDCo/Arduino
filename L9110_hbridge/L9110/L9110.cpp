df140bccbd392fa281ff42c39c76ae1e875a140f
Paul Allen <jp.paul.allen@gmail.com>
2014-05-20 18:50:01 -0600

    made additions to library

--- L9110_hbridge/L9110/L9110.cpp
+++ L9110_hbridge/L9110/L9110.cpp
@@ -22,7 +22,7 @@
 #include "Arduino.h"
 #include "L9110.h"
 
-void L9110::L9110()
+void L9110::L9110() // setup constructor for the library
 {
   // Pin assignment (Hard encoding these for now)
   // motor A
@@ -32,42 +32,41 @@ void L9110::L9110()
   int dirPinB8 = 8; // Hook to B-IA - Controls direction for motor B, high = forward, low = reverse
   int speedPinB10 = 10; // Hook to B-IB - Controls speed for motor B (PWM)
 
-  // Setup variables
-  int speedA; // 0 (stop) to 255 (full)
-  int speedB;
-  int dirA; // 0 (backward) or 1 (forward)
-  int dirB;
-
-  // Setup the pins
   pinMode(dirPinA, OUTPUT);
   pinMode(speedPinA, OUTPUT);
 
   pinMode(dirPinB, OUTPUT);
   pinMode(speedPinB, OUTPUT);
   
-  speedA = 0; // Set speedA to zero
-  speedB = 0; // Set speedB to zero
-  dirA = 0; //Set variable "dirA" to 1 (forward)
-  dirB = 0; //Set variable "dirB" to 1 (forward)
+//  speedA = 0; // Set speedA to zero
+//  speedB = 0; // Set speedB to zero
+//  dirA = 0; //Set variable "dirA" to 1 (forward)
+//  dirB = 0; //Set variable "dirB" to 1 (forward)
 }
 
-// Nothing to see past here until I finish setting up the above
-//
-//
-//
-//set motor speed
+
+void L9110::motorA(int dirA, int speedA) // Accept input for direction and speed for motor A
+{
+  
   analogWrite(speedPinA, speedA); //set speed{A|B} to "0" to come to full stop for that motor
-  analogWrite(speedPinB, speedB);
-//set direction
+
+  //set direction
   if (1 == dirA) { // Direction for motor A
     digitalWrite(dirPinA, HIGH); // Go forward
   } else {
     digitalWrite(dirPinA, LOW); // Go backward
   }
-  if (1 == dirB) { // Direction for motor B
+}
+
+void L9110::motorB(int dirB, int speedB) // Accept input for direction and speed for motor B
+{
+  
+  analogWrite(speedPinB, speedB); //set speed{A|B} to "0" to come to full stop for that motor
+
+  //set direction
+  if (1 == dirB) { // Direction for motor A
     digitalWrite(dirPinB, HIGH); // Go forward
   } else {
     digitalWrite(dirPinB, LOW); // Go backward
   }
-
-
+}
