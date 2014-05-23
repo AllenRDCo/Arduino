bfc6808fdaf59a95af8d2db7581ec31c7542c6f2
James Paul Allen <paul@madcat.allenrdco.com>
2014-05-23 11:13:30 -0600

    Added code for lipo monitor

--- L9110_hbridge/L9110_libtest/L9110_libtest.ino
+++ L9110_hbridge/L9110_libtest/L9110_libtest.ino
@@ -4,8 +4,8 @@
 
 L9110 L9110;
 
-  // setup pin for LiPo monitor
-  LiPoMonitor = 2
+// setup pin for LiPo monitor
+int LiPoMonitor = 2;
 
 
 void setup()
@@ -15,23 +15,23 @@ void setup()
 
 void loop()
 {
-  if (digitalRead(LiPoMonitor) == HIGH) 
-
-
-  L9110.motorA(1,255); //run both motors forward at full speed
-  L9110.motorB(1,255);
-  delay(5000);
-  L9110.motorA(0,255); //run both motors backward at full speed
-  L9110.motorB(0,255);
-  delay(5000);
-  L9110.motorA(1,127); //run both motors forward at half speed
-  L9110.motorB(1,127);
-  delay(5000);
-  L9110.motorA(1,255); //run motors at half speed in opposite directions
-  L9110.motorB(0,255);
-  delay(5000);
-  L9110.motorA(0,0); //full stop on both motors
-  L9110.motorB(0,0);
-  delay(3000);
+  if (digitalRead(LiPoMonitor) == LOW)
+  {
+    L9110.motorA(1,255); //run both motors forward at full speed
+    L9110.motorB(1,255);
+    delay(5000);
+    L9110.motorA(0,255); //run both motors backward at full speed
+    L9110.motorB(0,255);
+    delay(5000);
+    L9110.motorA(1,127); //run both motors forward at half speed
+    L9110.motorB(1,127);
+    delay(5000);
+    L9110.motorA(1,255); //run motors at half speed in opposite directions
+    L9110.motorB(0,255);
+    delay(5000);
+    L9110.motorA(0,0); //full stop on both motors
+    L9110.motorB(0,0);
+    delay(3000);
+  }
 }
 
